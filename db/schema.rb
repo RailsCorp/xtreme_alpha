cd693dfd51b15a855e3db48dc96f4fba160b5243
GokiSakuraba <goki14914@icloud.com>
2018-11-30 04:11:43 +0900

    add task table for db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_11_29_182156) do
+
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "content"
+    t.boolean "is_finished", default: false, null: false
+    t.datetime "deadline"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_tasks_on_title"
+  end
 
 end
