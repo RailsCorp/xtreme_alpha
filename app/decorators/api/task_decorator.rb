8a46784e1305d8846edf412bba87e4807d8c3e99
GokiSakuraba <goki14914@icloud.com>
2018-11-30 20:23:45 +0900

    reexecute rubocop for automode

--- app/decorators/api/task_decorator.rb
+++ app/decorators/api/task_decorator.rb
@@ -1,16 +1,18 @@
-class Api::TaskDecorator < Draper::Decorator
-  delegate_all
+module Api
+  class TaskDecorator < Draper::Decorator
+    delegate_all
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
+    # Define presentation-specific methods here. Helpers are accessed through
+    # `helpers` (aka `h`). You can override attributes, for example:
 
-  def created_at
-    helpers.content_tag :span, class: "time" do
-      object.created_at.strftime("%a %m/%d/%y")
+    def created_at
+      helpers.content_tag :span, class: "time" do
+        object.created_at.strftime("%a %m/%d/%y")
+      end
     end
-  end
 
-  def deadline
-    object.deadline.strftime("%a %y/%m/%d")
+    def deadline
+      object.deadline.strftime("%a %y/%m/%d")
+    end
   end
 end
