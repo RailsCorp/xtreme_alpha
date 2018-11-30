202891ed3ebde7aa365b6b18e26b139bde8fdf30
GokiSakuraba <goki14914@icloud.com>
2018-11-30 16:12:47 +0900

    add routing rspec for tasks

---
+++ spec/routes/api/tasks_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+describe "Tasks Routing", type: :routing do
+  it "GET index action" do
+    expect(get: "api/tasks").to route_to "api/tasks#index"
+  end
+  it "POST create action" do
+    expect(post: "api/tasks").to route_to "api/tasks#create"
+  end
+
+  it "PUT update action" do
+    expect(put: "api/tasks/1").to route_to(action: "update" ,controller: "api/tasks", id: "1")
+  end
+
+  it "DELETE destroy action" do
+    expect(delete: "api/tasks/1").to route_to(action: "destroy" ,controller: "api/tasks", id: "1")
+  end
+
+  it "GET show action" do
+    expect(get: "api/tasks/1").to route_to(action: "show" ,controller: "api/tasks", id: "1")
+  end
+end
