34073743436c8f6b085dacc08e10077847586016
GokiSakuraba <goki14914@icloud.com>
2018-12-04 03:08:32 +0900

    add routing spec for passwords

---
+++ spec/routes/devise/passwords_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+describe "Devise Passwords Routing", type: :routing do
+  it "GET api/auth/password/new new action" do
+    expect(get: "api/auth/password/new").to route_to(action: "new", controller: "devise_token_auth/passwords")
+  end
+
+  it "GET api/auth/password/edit edit action" do
+    expect(get: "api/auth/password/edit").to route_to(action: "edit", controller: "devise_token_auth/passwords")
+  end
+
+  it "POST api/auth/password create action" do
+    expect(post: "api/auth/password").to route_to(action: "create", controller: "devise_token_auth/passwords")
+  end
+
+  it "PUT api/auth/password destroy action" do
+    expect(put: "api/auth/password").to route_to(action: "update", controller: "devise_token_auth/passwords")
+  end
+end
