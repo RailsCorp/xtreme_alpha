30496bc620904e072568e294462daecc33172b0a
GokiSakuraba <goki14914@icloud.com>
2018-12-03 17:52:42 +0900

    add configuration for debug

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module XtremeAlpha
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.time_zone = ENV["TZ"]
+    config.debug_exception_response_format = :default
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "*.{rb,yml}").to_s]
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
