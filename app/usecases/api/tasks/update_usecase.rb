01c7ff694b6367f28f7ed8243c89a0594f149878
GokiSakuraba <goki14914@icloud.com>
2018-12-03 14:15:24 +0900

    add usecase for task update action for task

---
+++ app/usecases/api/tasks/update_usecase.rb
@@ -0,0 +1,30 @@
+module Api
+  module Tasks
+    class UpdateUsecase
+      def initialize(params , task)
+        @params = params
+        @task = task
+      end
+
+      def execute
+        if @params[:is_finished] == "true"
+          success_task_log = SuccessTaskLog.new(task_id: @task.id, title: @task.title)
+          success_task_log.save!
+          return { is_finished: true, message: I18n.t("models.messages.success_task_logs") }
+        else
+          update_usecase
+        end
+      end
+
+      private
+
+      def update_usecase
+        if @task.update!(@params)
+          Api::TaskDecorator.decorate(@task)
+        else
+          @task.errors.full_messages
+        end
+      end
+    end
+  end
+end
