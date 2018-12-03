ac0883f0758123dff989ecb7e9e889cd2b040f13
GokiSakuraba <goki14914@icloud.com>
2018-12-03 17:54:13 +0900

    add routing for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :api do
+  namespace :api, defaults: { format: :json } do
+    mount_devise_token_auth_for "User", at: "auth", controllers: {
+      registrations: "api/auth/registrations"
+    }
     resources :tasks, only: %i[index create destroy update show]
   end
 end
