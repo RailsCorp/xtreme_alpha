0b802b550635efcdeebbf8b0169ccb929006d6b2
GokiSakuraba <goki14914@icloud.com>
2018-12-04 13:50:46 +0900

    add tokens in db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 2018_12_03_093330) do
     t.string "last_sign_in_ip"
     t.string "name"
     t.string "email"
-    t.text "tokens", null: false
+    t.json "tokens", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
