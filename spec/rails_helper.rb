7fdc0645c2403f51f0a2fa4ce5885cea2fcc43f8
GokiSakuraba <goki14914@icloud.com>
2018-12-06 02:24:14 +0900

    fix somes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path("../config/environment", __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
 require "devise"
+require "devise_token_auth"
 require "faker"
 require "line/bot"
 require "simplecov"
