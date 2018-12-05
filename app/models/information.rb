1af2983274ac266d8974dfc61bbddce2bf435aac
GokiSakuraba <goki14914@icloud.com>
2018-12-06 04:37:02 +0900

    add model for information

--- app/models/information.rb
+++ app/models/information.rb
@@ -1,4 +1,8 @@
 class Information < ApplicationRecord
   enum info_type: %i[personal groups]
+
+  has_one :info_user
+  has_one :user, through: :info_user
+
   validates :info_type, presence: true
 end
