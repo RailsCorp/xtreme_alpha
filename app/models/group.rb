3defecd362fbdb5eda4826230dbb1bf8e16d491a
GokiSakuraba <goki14914@icloud.com>
2018-12-05 19:36:13 +0900

    add association between group and user

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,9 @@
 class Group < ApplicationRecord
   enum group_type: %i[students company]
+
+  has_many :members, dependent: :destroy
+  has_many :users, through: :members
+
   validates :name, presence: true
   validates :group_type, presence: true, numericality: true
 end
