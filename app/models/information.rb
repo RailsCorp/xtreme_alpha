9313e1eec3394ea1753ad8456eea2706d5b8eae6
GokiSakuraba <goki14914@icloud.com>
2018-12-06 05:19:36 +0900

    add association for group

--- app/models/information.rb
+++ app/models/information.rb
@@ -1,8 +1,10 @@
 class Information < ApplicationRecord
   enum info_type: %i[personal groups]
 
-  has_one :info_user
+  has_one :info_user, dependent: :destroy
   has_one :user, through: :info_user
+  has_one :info_group, dependent: :destroy
+  has_one :group, through: :info_group
 
   validates :info_type, presence: true
 end
