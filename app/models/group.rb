cb893dfbf37eef01632e20f67487f2a67afe2cbd
GokiSakuraba <goki14914@icloud.com>
2018-12-06 02:20:49 +0900

    add model for group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,9 +1,11 @@
 class Group < ApplicationRecord
-  enum group_type: %i[students company]
+  enum group_type: %i[students companies]
+  # enum group_type: { students: 0, companies: 1 }
 
   has_many :members, dependent: :destroy
   has_many :users, through: :members
+  has_one_attached :image
 
   validates :name, presence: true
-  validates :group_type, presence: true, numericality: true
+  # validates :group_type, presence: true, numericality: true
 end
