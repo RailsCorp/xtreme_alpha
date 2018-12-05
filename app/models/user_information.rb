a0e8dddfe4c1dc316c821353089922e6b227a86c
GokiSakuraba <goki14914@icloud.com>
2018-12-06 04:50:21 +0900

    fix association for model user_information

--- app/models/user_information.rb
+++ app/models/user_information.rb
@@ -1,7 +1,7 @@
 class UserInformation < ApplicationRecord
   has_one_attached :image
 
-  belongs_to :user
+  belongs_to :information
 
   validates :phone_number, presence: true, format: { with: Constants::VALID_PHONE_REGEX }
 end
