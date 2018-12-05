25574bee75359aad31a0640e6632d58200c2601f
GokiSakuraba <goki14914@icloud.com>
2018-12-06 02:29:42 +0900

    add spec routing for groups

---
+++ spec/routes/api/groups_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+describe "Groups Routing", type: :routing do
+  it "POST create action" do
+    expect(post: "api/groups").to route_to(action: "create", controller: "api/groups")
+  end
+
+  it "PUT update action" do
+    expect(put: "api/groups/1").to route_to(action: "update", controller: "api/groups", id: "1")
+  end
+
+  it "DELETE destroy action" do
+    expect(delete: "api/groups/1").to route_to(action: "destroy", controller: "api/groups", id: "1")
+  end
+
+  it "GET show action" do
+    expect(get: "api/groups/1").to route_to(action: "show", controller: "api/groups", id: "1")
+  end
+end
