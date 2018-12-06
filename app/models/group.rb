ec3d879f47f11a0ea9e6370bdf02f01b27a825eb
GokiSakuraba <goki14914@icloud.com>
2018-12-07 03:18:49 +0900

    add association for group

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,6 +4,8 @@ class Group < ApplicationRecord
 
   has_many :members, dependent: :destroy
   has_many :users, through: :members
+  has_many :task_groups, dependent: :destroy
+  has_many :tasks, through: :task_groups
   has_one :info_group, dependent: :destroy
   has_one :information, through: :info_group
   has_one_attached :image
