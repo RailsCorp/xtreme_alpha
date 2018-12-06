d217cf1c442709ff326d2d3c4ac4d30580d5abfb
GokiSakuraba <goki14914@icloud.com>
2018-12-07 03:19:37 +0900

    add association for task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,8 @@ class Task < ApplicationRecord
   has_many :success_task_logs, dependent: :destroy
   has_many :task_users, dependent: :destroy
   has_many :users, through: :task_users
+  has_many :task_groups, dependent: :destroy
+  has_many :groups, through: :task_groups
 
   validates :title, presence: true
   validates :is_finished, inclusion: [true, false]
