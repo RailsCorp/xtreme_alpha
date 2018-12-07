edcd7354d64dd0e27acca98921697fd78a353543
GokiSakuraba <goki14914@icloud.com>
2018-12-08 03:18:43 +0900

    fix spec for routing

--- spec/routes/api/groups_spec.rb
+++ spec/routes/api/groups_spec.rb
@@ -2,18 +2,18 @@ require "rails_helper"
 
 describe "Groups Routing", type: :routing do
   it "POST create action" do
-    expect(post: "api/groups").to route_to(action: "create", controller: "api/groups")
+    expect(post: "api/groups").to route_to(action: "create", controller: "api/groups", format: "json")
   end
 
   it "PUT update action" do
-    expect(put: "api/groups/1").to route_to(action: "update", controller: "api/groups", id: "1")
+    expect(put: "api/groups/1").to route_to(action: "update", controller: "api/groups", id: "1", format: "json")
   end
 
   it "DELETE destroy action" do
-    expect(delete: "api/groups/1").to route_to(action: "destroy", controller: "api/groups", id: "1")
+    expect(delete: "api/groups/1").to route_to(action: "destroy", controller: "api/groups", id: "1", format: "json")
   end
 
   it "GET show action" do
-    expect(get: "api/groups/1").to route_to(action: "show", controller: "api/groups", id: "1")
+    expect(get: "api/groups/1").to route_to(action: "show", controller: "api/groups", id: "1", format: "json")
   end
 end
