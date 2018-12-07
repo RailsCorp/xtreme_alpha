3a80270213e9a3266c2d3fab985d9a2c9715478f
GokiSakuraba <goki14914@icloud.com>
2018-12-08 03:25:04 +0900

    fix spec routing for user

--- spec/routes/api/users_spec.rb
+++ spec/routes/api/users_spec.rb
@@ -2,6 +2,6 @@ require "rails_helper"
 
 describe "Users Routing", type: :routing do
   it "GET index action" do
-    expect(get: "api/users/1").to route_to(action: "show", controller: "api/users", id: "1")
+    expect(get: "api/users/1").to route_to(action: "show", controller: "api/users", id: "1", format: "json")
   end
 end
