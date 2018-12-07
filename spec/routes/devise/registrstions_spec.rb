4574f1af6cf0d5637fed4051405c9ad27b3e13c0
GokiSakuraba <goki14914@icloud.com>
2018-12-08 03:25:44 +0900

    add format json for spec rputing in devise

--- spec/routes/devise/registrstions_spec.rb
+++ spec/routes/devise/registrstions_spec.rb
@@ -2,27 +2,27 @@ require "rails_helper"
 
 describe "Devise Registrations Routing", type: :routing do
   it "GET api/auth/cancel cancel action" do
-    expect(get: "api/auth/cancel").to route_to(action: "cancel", controller: "api/auth/registrations")
+    expect(get: "api/auth/cancel").to route_to(action: "cancel", controller: "api/auth/registrations", format: "json")
   end
 
   it "GET api/auth/sign_up new action" do
-    expect(get: "api/auth/sign_up").to route_to(action: "new", controller: "api/auth/registrations")
+    expect(get: "api/auth/sign_up").to route_to(action: "new", controller: "api/auth/registrations", format: "json")
   end
 
   it "GET api/auth/edit edit action" do
-    expect(get: "api/auth/edit").to route_to(action: "edit", controller: "api/auth/registrations")
+    expect(get: "api/auth/edit").to route_to(action: "edit", controller: "api/auth/registrations", format: "json")
   end
 
   it "POST api/auth/sign_in create action" do
-    expect(post: "api/auth").to route_to(action: "create", controller: "api/auth/registrations")
+    expect(post: "api/auth").to route_to(action: "create", controller: "api/auth/registrations", format: "json")
   end
 
   it "DELETE api/auth/sign_out destroy action" do
-    expect(delete: "api/auth").to route_to(action: "destroy", controller: "api/auth/registrations")
+    expect(delete: "api/auth").to route_to(action: "destroy", controller: "api/auth/registrations", format: "json")
   end
 
   it "PUT api/auth/sign_out destroy action" do
-    expect(put: "api/auth").to route_to(action: "update", controller: "api/auth/registrations")
+    expect(put: "api/auth").to route_to(action: "update", controller: "api/auth/registrations", format: "json")
   end
 
   it "GET api/auth/validate_token new action" do
