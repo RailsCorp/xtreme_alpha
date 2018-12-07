2e4fbb96f6c4d82cce488d3711cc883c4618e8bd
GokiSakuraba <goki14914@icloud.com>
2018-12-08 03:24:32 +0900

    add spec routing for api/teams

--- spec/routes/api/teams_spec.rb
+++ spec/routes/api/teams_spec.rb
@@ -2,21 +2,21 @@ require "rails_helper"
 
 describe "Teams Routing", type: :routing do
   it "GET index action" do
-    expect(get: "api/groups/1/teams").to route_to(action: "index", controller: "api/teams", group_id: "1")
+    expect(get: "api/groups/1/teams").to route_to(action: "index", controller: "api/teams", group_id: "1", format: "json")
   end
   it "POST create action" do
-    expect(post: "api/groups/1/teams").to route_to(action: "create", controller: "api/teams", group_id: "1")
+    expect(post: "api/groups/1/teams").to route_to(action: "create", controller: "api/teams", group_id: "1", format: "json")
   end
 
   it "PUT update action" do
-    expect(put: "api/groups/1/teams/1").to route_to(action: "update", controller: "api/teams", group_id: "1", id: "1")
+    expect(put: "api/groups/1/teams/1").to route_to(action: "update", controller: "api/teams", group_id: "1", id: "1", format: "json")
   end
 
   it "DELETE destroy action" do
-    expect(delete: "api/groups/1/teams/1").to route_to(action: "destroy", controller: "api/teams", group_id: "1", id: "1")
+    expect(delete: "api/groups/1/teams/1").to route_to(action: "destroy", controller: "api/teams", group_id: "1", id: "1", format: "json")
   end
 
   it "GET show action" do
-    expect(get: "api/groups/1/teams/1").to route_to(action: "show", controller: "api/teams", group_id: "1", id: "1")
+    expect(get: "api/groups/1/teams/1").to route_to(action: "show", controller: "api/teams", group_id: "1", id: "1", format: "json")
   end
 end
