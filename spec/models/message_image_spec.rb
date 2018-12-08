2caa9fdacbaf90c6fea848216475a7b79250b2c1
GokiSakuraba <goki14914@icloud.com>
2018-12-09 04:27:12 +0900

    add model spec fir message_image

---
+++ spec/models/message_image_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe MessageImage, type: :model do
+  context "validation" do
+    subject { create(:message_text) }
+    it { is_expected.to validate_presence_of(:message_id) }
+  end
+
+  context "db" do
+    subject { create(:message_text) }
+    it { is_expected.to have_db_column(:message_id).with_options(null: false) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
+
+    it { should have_db_index(:message_id) }
+
+    it { is_expected.to belong_to(:message) }
+  end
+end
