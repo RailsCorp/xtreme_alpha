013ffaa1b27c743779d8082ae83e152a78914097
GokiSakuraba <goki14914@icloud.com>
2018-12-09 04:25:56 +0900

    add message_text_spec

--- spec/models/message_text_spec.rb
+++ spec/models/message_text_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe MessageText, type: :model do
   context "db" do
     subject { create(:message_text) }
     it { is_expected.to have_db_column(:message_id).with_options(null: false) }
+    it { is_expected.to have_db_column(:text).with_options(null: false) }
     it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
     it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
 
