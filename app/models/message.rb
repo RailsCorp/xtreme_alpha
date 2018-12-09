a78083249ae524d653928a5018c42a5bc786327a
GokiSakuraba <goki14914@icloud.com>
2018-12-10 01:47:10 +0900

    add model for message

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,8 +4,14 @@ class Message < ApplicationRecord
   has_one :message_text, dependent: :destroy
   has_one :message_file, dependent: :destroy
   has_one :message_image, dependent: :destroy
+
   enum message_type: %i[text image file]
+
   validates :user_id, presence: true
   validates :team_id, presence: true
   validates :message_type, presence: true
+
+  scope :with_message_text, -> { joins(:message_text) }
+  scope :with_message_image, -> { joins(:message_image) }
+  scope :with_message_file, -> { joins(:message_file) }
 end
