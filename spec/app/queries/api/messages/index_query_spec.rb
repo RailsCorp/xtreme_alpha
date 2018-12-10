cac217d2aac3972d74b8db7d713f608f45d75cce
GokiSakuraba <goki14914@icloud.com>
2018-12-10 13:55:04 +0900

    add query for message index

--- spec/app/queries/api/messages/index_query_spec.rb
+++ spec/app/queries/api/messages/index_query_spec.rb
@@ -2,34 +2,44 @@ require "rails_helper"
 
 describe Api::Messages::IndexQuery do
   let(:image) { Rack::Test::UploadedFile.new(File.join(Rails.root, "spec/images/test.jpg")) }
+  let(:file) { Rack::Test::UploadedFile.new(File.join(Rails.root, "spec/images/test.pdf")) }
   describe "Api::Messages::IndexQuery" do
+    before do
+      @user = create(:user)
+      @group = create(:group)
+      create(:member, user_id: @user.id, group_id: @group.id)
+      @team = create(:team, group_id: @group.id)
+    end
     it "MessageText Success" do
-      user = create(:user)
-      group = create(:group)
-      create(:member, user_id: user.id, group_id: group.id)
-      team = create(:team, group_id: group.id)
       3.times do |_time|
-        message = create(:message, team_id: team.id, user_id: user.id, message_type: "text")
+        message = create(:message, team_id: @team.id, user_id: @user.id, message_type: "text")
         create(:message_text, message_id: message.id, text: "hello")
       end
-      messages = described_class.new(team.id).execute
+      messages = described_class.new(@team.id).execute
       expect(messages.count).to eq 3
       expect(messages[0].message_text.text).to eq("hello")
     end
 
-    it "MessageImage" do
-      user = create(:user)
-      group = create(:group)
-      create(:member, user_id: user.id, group_id: group.id)
-      team = create(:team, group_id: group.id)
+    it "MessageImage Success" do
       3.times do |_time|
-        message = create(:message, team_id: team.id, user_id: user.id, message_type: "image")
+        message = create(:message, team_id: @team.id, user_id: @user.id, message_type: "image")
         create(:message_image, message_id: message.id, image: image)
       end
-      messages = described_class.new(team.id).execute
+      messages = described_class.new(@team.id).execute
       expect(messages.count).to eq 3
       expect(messages[0].image.filename).to eq("test.jpg")
       expect(messages[0].image.name).to eq("image")
+    end
+
+    it "MessageFile Success" do
+      3.times do |_time|
+        message = create(:message, team_id: @team.id, user_id: @user.id, message_type: "image")
+        create(:message_file, message_id: message.id, file: file)
+      end
+      messages = described_class.new(@team.id).execute
+      expect(messages.count).to eq 3
+      expect(messages[0].file.filename).to eq("test.pdf")
+      expect(messages[0].file.name).to eq("file")
     end
   end
 end
